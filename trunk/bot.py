5fe91c90ab8e55c5d6f64d31f4a61ad587c32f1f
gordon.hugh.martin@gmail.com <gordon.hugh.martin@gmail.com@9a87762b-36a6-3fb7-8711-ff58fecdcaec>
2011-04-10 14:12:13 +0000

    started writing update feature

--- trunk/bot.py
+++ trunk/bot.py
@@ -1,6 +1,6 @@
 import threading
 import Input
-import Output 
+import Output
 from Irc.irc import Irc
 from datetime import datetime
 import re # Regular expressions
@@ -13,75 +13,75 @@ class Observer:
 class Bot(Observer):
     """
     Simple IRC bot.
-    
+
     Respond to chatter by registering callbacks which
     are triggered by a regular expression pattern match.
     """
-    
+
     def __init__(self, server, channel, nick, name):
         self.irc = Irc()
         self.irc.connect(server)
         self.irc.set_info(nick, nick)
-        
+
         self.channel = channel
-        
+
         self.irc.join(channel)
         self.irc.me(channel, "has initiated self-destruct sequence!")
-    
+
         self.input = Input.Input(self.irc)
         self.input.registerObserver(self)
         self.input.start()
-        
+
         self.output = Output.Output(self.irc)
         self.output.start()
-        
+
         self.callbacks = {}
         self.activity = {} # Maps usernames to activity
-    
-    
+
+
     def notify(self, data):
         """
         Respond to notification that some event being
         observed has occurred.
-        
+
         Compares the given message against a collection
         of patterns and executes any appropriate callbacks.
-        
+
         * data = {"from", "to", "message"}
         """
         for pattern in self.callbacks.keys():
             if re.match(pattern, data["message"]) != None:
                 self.callbacks[pattern](self, data)
                 break
-                
+
         self.activity[data["from"]] = datetime.now()
-                
-    
+
+
     def register(self, pattern, callback):
         """
         Registers a callback for an input pattern.
         """
         self.callbacks[pattern] = callback
-    
-    
+
+
     def send(self, msg, channel=""):
         """
         Sends a message to specified channel.
         """
         if channel == "": channel = self.channel
-        
+
         self.irc.send(channel, msg)
-    
-    
+
+
     def me(self, msg, channel=""):
         """
         Sends a /me message to specified channel.
         """
         if channel == "": channel = self.channel
-        
+
         self.irc.me(channel, msg)
-    
-    
+
+
 # Callbacks
 def umad(bot, data):
     bot.send("U mad?")
@@ -89,7 +89,7 @@ def umad(bot, data):
 
 def friday(bot, data):
     bot.send("Friday, friday, gotta get down on Friday!")
-    
+
 
 def goofed(bot, data):
     bot.send("Sorry, %s" % data["from"])
@@ -98,13 +98,13 @@ def goofed(bot, data):
 def wikisearch(bot, data):
     query = data["message"].replace("!wiki ", "")
     results = wiki.wikiSearch(query)
-    
+
     if (results == []):
         bot.send("No wikipedia results for \"%s\"" % query)
         return
-    
+
     bot.send("Wikipedia results for \"%s\":" % query)
-    
+
     for result in results:
         bot.send("* %s: %s" % (result[0], result[1]))
 
@@ -115,23 +115,23 @@ def slap(bot, data):
 
 def seen(bot, data):
     user = data["message"].replace("!seen ", "")
-    
+
     if user not in bot.activity:
         bot.send("I haven't seen %s around here" % user)
     else:
         lastSeen = datetime.now() - bot.activity[user]
-        
+
         days = lastSeen.days
         mins = lastSeen.seconds / 60
         hours = mins / 60
-        
+
         daysStr = ("%d days, " % days) if days > 0 else ""
         hoursStr = ("%d hours and " % hours) if hours > 0 else ""
         timeAgo = "%s%s%d minutes" % (daysStr, hours, mins)
-        
+
         bot.send("%s was last seen %s ago" % (user, timeAgo))
 
-        
+
 def moo(bot, data):
     bot.send("         (__)")
     bot.send("         (oo)")
@@ -139,7 +139,13 @@ def moo(bot, data):
     bot.send(" / |     ||")
     bot.send("*  ||----||")
     bot.send("   ~~    ~~")
-    
+
+def update(bot, data):
+    if (data["from"] in self.input.owners):
+
+
+
+
 
 # Main function
 def main():
@@ -147,9 +153,9 @@ def main():
     nick = "gucs-bot"
     channel = "##GUCS"
     name = "Sir Trollface Esq."
-    
+
     gucsbot = Bot(server, channel, nick, name)
-    
+
     # Register callbacks to give gucs-bot something to do
     gucsbot.register("(p|P)roblem\?", umad)
     gucsbot.register("(f|F)riday", friday)
@@ -158,6 +164,7 @@ def main():
     gucsbot.register("!slap \w", slap)
     gucsbot.register("!seen \w", seen)
     gucsbot.register("(m|M)ooo*", moo)
-    
+    gucsbot.register("!update", update)
+
 if __name__ == "__main__":
     main()
