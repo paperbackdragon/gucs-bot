47caf15b162404411b98f8daa5f41e0854281297
mistercouk <mistercouk@9a87762b-36a6-3fb7-8711-ff58fecdcaec>
2011-12-17 17:23:15 +0000

    Fixed channel name.

--- trunk/spawn.py
+++ trunk/spawn.py
@@ -1,4 +1,4 @@
 import subprocess
 
 print subprocess.call(["python", "bot.py",
-                       "-sirc.freenode.org", "-ngucs-bot", "#monthen"])
+                       "-sirc.freenode.org", "-ngucs-bot", "##gucs"])
